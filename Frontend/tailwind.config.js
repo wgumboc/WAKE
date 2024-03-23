4efc6df2cfbcebf95de30bec1cdf48eb46d6dac6
Alethea Kramer <aletheakramer@outlook.com>
2024-03-23 14:05:44 -0700

    fixed install

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -3,8 +3,6 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./path/to/your/html/files/**/*.html",
-    "./path/to/your/js/files/**/*.js",
   ],
   theme: {
     extend: {},
