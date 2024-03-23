9b2f83a77eb1814b050f41a10e33d3773ac369de
William <wgumboc@gmail.com>
2024-03-23 14:40:39 -0700

    Adding styling for navbar visibility.

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 
 const Navbar = () => {
-
-
-
   return (
       <div className="navbar bg-base-100">
-          <a className="btn btn-ghost text-xl"></a>
+          <a className="btn btn-ghost text-xl">Title</a>
       </div>
+
   )
 }
 
