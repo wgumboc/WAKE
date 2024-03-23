0929c09bcf9f856b0b7218066d14facd4342ad38
Alethea Kramer <aletheakramer@outlook.com>
2024-03-23 16:45:23 -0700

    Added long answer question

--- Frontend/src/components/Question3.jsx
+++ Frontend/src/components/Question3.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import '.././App.css'
 
 const Question3 = () => {
   return (
-    <div>Question3</div>
+    <div className="w-full">
+
+
+      <div className="card bg-base-100 shadow-xl">
+        <div className="card-body">
+        <h2 className="h2"> Question 3</h2>
+        <h3 className="h3">This is the question</h3>
+        <textarea className="textarea textarea-bordered w-full" placeholder="Answer question here"></textarea>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Save</button>
+          </div>
+        </div>
+        </div>
+    </div>
+    
   )
 }
 
